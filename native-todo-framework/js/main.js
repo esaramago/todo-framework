7633134c2b9c3cf5fe4d5350ee3cd8c384fcccbe
Emanuel Saramago <emanuel.reg@outlook.com>
2019-11-23 00:55:38 +0000

    native done

--- native-todo-framework/js/main.js
+++ native-todo-framework/js/main.js
@@ -1,96 +1,20 @@
 import header from "./components/header.js";
 import icon from "./components/icon.js";
-import listItem from "./components/list-item.js";
-//import list from "./components/list.js";
+import todo from "./components/todo.js";
 
 const App = function() {
 
-    /* localStorage.setItem('todo-items', JSON.stringify([
-        {
-            id: 1,
-            text: 'Lorem ipsum 1',
-            isDone: false
-        }
-    ])); */
-
-    var data = [];
+    var todo = document.getElementById('todo');
 
     function init() {
         var storage = JSON.parse(localStorage.getItem('todo-items'));
         if (storage) {
-            data = storage;
-            renderList();
-        }
-        document.querySelector('.js-form').addEventListener('submit', addItem);
-    }
-
-
-    function renderList() {
-        const listElm = document.querySelector('.js-list');
-        var list = '';
-        data.forEach(item => {
-            var isDone = item.isDone ? 'done' : '';
-            list += `<li is="c-list-item" id="${item.id}" ${isDone}>${item.text}</li>`;
-        });
-        listElm.innerHTML = list;
-
-
-        document.querySelectorAll('.js-remove').forEach(button => {
-            button.addEventListener('click', removeItem);
-        });
-    }
-
-
-    function addItem(e) {
-
-        e.preventDefault();
-        
-        setItem();
-    }
-    function setItem() {
-
-        var input = document.getElementById('add');
-        var text = input.value;
-
-        if (text) {
-
-            var id = Math.random();
-            
-            // check if id already exists
-            var isRepeated = data.find(x => x.id == id);
-            
-            if (isRepeated) {
-                setItem();
-            }
-            else {
-                data.push({
-                    id: id,
-                    text: text,
-                    isDone: false
-                });
-                
-                renderList(); // re-render list
-                saveLocalStorage();
-
-                input.value = '';
-            }
-        }
-    }
-    
-    function removeItem() {
-        var id = this.dataset.id;
-        var item = data.find(x => x.id == id);
-        var index = data.indexOf(item);
-        if(index > -1) {
-            data.splice(index, 1);
-            renderList(); // re-render list
-            saveLocalStorage();
+            todo.items = storage; // setter - sets todo component data
+            todo.items; // getter - runs todo component data for the first time
         }
     }
 
-    function saveLocalStorage() {
-        localStorage.setItem('todo-items', JSON.stringify(data));
-    }
+
  
     return {
         init: init
